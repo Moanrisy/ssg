0916bd6c7094f29514b1fdb6c4b923f57d728f52
Moanrisy <moanrisy@gmail.com>
2024-01-21 23:25:33 +0700

    Feat: Add ClearTerminal function

--- shared/message.go
+++ shared/message.go
@@ -1,5 +1,11 @@
 package shared
 
+import (
+	"os"
+	"os/exec"
+	"runtime"
+)
+
 type ContentType int
 
 const (
@@ -10,4 +16,21 @@ const (
 type Message struct {
 	Content string
 	Type    ContentType
+}
+
+func ClearTerminal() {
+	var cmd *exec.Cmd
+
+	switch runtime.GOOS {
+	case "windows":
+		cmd = exec.Command("cmd", "/c", "cls")
+	case "darwin", "linux":
+		cmd = exec.Command("clear")
+	default:
+		// Unsupported operating system
+		return
+	}
+
+	cmd.Stdout = os.Stdout
+	cmd.Run()
 }
